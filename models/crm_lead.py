c5891baf0404f431b558b21599ab22175b58b276
Bottle1985 <mrboongvn@yahoo.com.vn>
2024-03-07 08:49:10 +0700

    - Updated lead sale can save

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -2,15 +2,18 @@
 
 from odoo import models, fields, api
 from odoo.exceptions import UserError
-
+import logging
+_logger = logging.getLogger(__name__)
 
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
-
-    def write(self, vals):
-        if self.user_id != self.env.user:
+    
+    def write(self, vals):      
+        team_lead = self.env['crm.team'].search([('user_id', '=', self.env.uid)])            
+        # if self.user_id != self.env.user:
+        if self.user_id != self.env.user and len(team_lead)==0:
             raise UserError("You cannot edit Opp/Lead of other person")
         else:
             return super(CrmLead, self).write(vals)
 
-
+        # _logger.info('Tala and Boong Check save lead', len(team_lead))
