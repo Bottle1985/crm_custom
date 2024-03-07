ce2016c958b82c213fc909ad701b3f42cfa33c4c
Bottle1985 <mrboongvn@yahoo.com.vn>
2024-03-07 10:31:11 +0700

    - Updated not save when have admission

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -9,11 +9,11 @@ class CrmLead(models.Model):
     _inherit = 'crm.lead'
     
     def write(self, vals):      
-        team_lead = self.env['crm.team'].search([('user_id', '=', self.env.uid)])            
+        team_lead = self.env['crm.team'].search([('user_id', '=', self.env.uid)])
         # if self.user_id != self.env.user:
-        if self.user_id != self.env.user and len(team_lead)==0:
+        if self.user_id != self.env.user and len(team_lead)==0 and 'admission_id' not in vals:
             raise UserError("You cannot edit Opp/Lead of other person")
         else:
             return super(CrmLead, self).write(vals)
 
-        # _logger.info('Tala and Boong Check save lead', len(team_lead))
+
