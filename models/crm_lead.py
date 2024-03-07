bd938235e56a8da61139700ff5415ae684f6c04f
Bottle1985 <mrboongvn@yahoo.com.vn>
2024-03-07 14:34:04 +0700

    + Added total grade condition

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -11,9 +11,7 @@ class CrmLead(models.Model):
     def write(self, vals):      
         team_lead = self.env['crm.team'].search([('user_id', '=', self.env.uid)])
         # if self.user_id != self.env.user:
-        if self.user_id != self.env.user and len(team_lead)==0 and 'admission_id' not in vals:
+        if self.user_id != self.env.user and len(team_lead)==0 and 'x_total_grade' not in vals:
             raise UserError("You cannot edit Opp/Lead of other person")
         else:
             return super(CrmLead, self).write(vals)
-
-
