b4eb45caab8c193f88f4b9933541f8d13c1ab2a3
Bottle1985 <mrboongvn@yahoo.com.vn>
2024-03-28 15:41:36 +0700

    + Added remain fee

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -13,13 +13,15 @@ class CrmLead(models.Model):
         inverse_name='payment_id',
         string='Payment Lines'
     ) 
-    tuition_ids = fields.Many2one('crm.cost', string='Tuitions', compute='_load_payment_fee')
+    # tuition_ids = fields.Many2one('crm.cost', string='Tuitions', compute='_load_payment_fee')
+    tuition_ids = fields.Many2one('crm.cost', string='Tuitions', help="Tổng tiền đóng = Lệ phí nhập học + Lệ phí khám sức khỏe + Bảo hiểm y tế + Tổng học phí học kì 1")
     save_cost_id = fields.Many2one('crm.save.cost', string='Save Cost')
     
     amount_total = fields.Float(string="Total Amount", store=True, compute='_compute_amounts')
     total_fee = fields.Float(string='Total Fee', compute='_compute_total_fees')
-   
-    
+    reduced_fee = fields.Float(string='Reduced Fee', compute='_compute_reduced_fees')
+    remain_fee = fields.Float(string='Remain Fee', compute='_compute_remain_fees')
+      
     """Compute the total amounts of the fee payment."""
     @api.depends('table_payment.so_tien')
     def _compute_amounts(self):
@@ -36,16 +38,31 @@ class CrmLead(models.Model):
             total_fee = 0.0
             tuition = student.tuition_ids
             if tuition:
-                total_fee = tuition.admission_fee + tuition.health_check_fee + tuition.insurance
+                total_fee = tuition.admission_fee + tuition.health_check_fee + tuition.insurance + tuition.total_course_cost
             student.total_fee = total_fee  
-    """Load all payment fee to table. This function set table to readonly"""             
-    def _load_payment_fee(self):  
-        first_payment = self.env['crm.cost'].search([], limit=1, order='id')
-        if first_payment: 
-            self.tuition_ids = first_payment.id
-        else:
-            self.tuition_ids = False    
-
+    # """Load all payment fee to table. This function set table to readonly"""             
+    # def _load_payment_fee(self):  
+        # first_payment = self.env['crm.cost'].search([], limit=1, order='id')
+        # if first_payment: 
+            # self.tuition_ids = first_payment.id
+        # else:
+            # self.tuition_ids = False    
+    """Compute total fee reduced when have scholarship"""
+    @api.depends('save_cost_id')
+    def _compute_reduced_fees(self):
+       for record in self:
+            reduced_fee = 0.0
+            if record.save_cost_id:
+                reduced_fee = record.save_cost_id.corporate_scholarship + record.save_cost_id.admission_offer
+         
+            record.reduced_fee = reduced_fee
+    """Compute the remain fee to pay"""        
+    @api.depends('total_fee', 'amount_total', 'reduced_fee')
+    def _compute_remain_fees(self):
+        for record in self:
+            remain_fee = record.total_fee - record.amount_total - record.reduced_fee
+            record.remain_fee = remain_fee
+            
     def write(self, vals):      
         if self.user_id != self.env.user and not self.env.user.has_group('sales_team.group_sale_manager'):
             raise UserError("You cannot edit Opp/Lead of other person")
